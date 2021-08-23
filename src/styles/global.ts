cdf7397c878471a94b2d3f595fc8b611e9ad14fc
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-23 19:39:17 -0300

    finish font configuration

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -39,6 +39,24 @@ const reset = css`
     -webkit-font-smoothing: antialiased;
   }
 
+  body,
+  input,
+  textarea,
+  button {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h5,
+  strong {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }
